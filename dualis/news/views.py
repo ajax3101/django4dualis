910b2871d97bb23c4c6b59fbef0b5bd6e45e1bdd
Denys Morozov <ajax3101@gmail.com>
2022-05-27 01:18:58 +0300

    localize change to ukraine

--- dualis/news/views.py
+++ dualis/news/views.py
@@ -1,12 +1,11 @@
+from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 
 def index(request):
-    print(dir(request))
-    return HttpResponse('hello world')
-
-
-def test(request):
-    #print(dir(request))
-    return HttpResponse('<h1>TEST!!!!!!!!!</h1>')
+    post = Post.objects.all()
+    context = {'news': post, 'title': 'Список новин'}
+    return render(request, 'news/index.html', context)
