065a72f97c2aeb3a327b67ce68648d8c9cfdb532
Denys Morozov <ajax3101@gmail.com>
2022-05-28 22:15:03 +0300

    add category

--- dualis/news/urls.py
+++ dualis/news/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
     path('', index),
-    
+    path('category/<int:category_id>/', get_category),
 ]
--- dualis/news/views.py
+++ dualis/news/views.py
@@ -1,11 +1,25 @@
 from turtle import title
+from unicodedata import category
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Post
+from .models import Post, Category
 
 
 def index(request):
     post = Post.objects.all()
-    context = {'news': post, 'title': 'Список новин'}
+    categories = Category.objects.all()
+
+    context = {'news': post, 
+                'title': 'Список новин', 
+                'categories': categories, }
     return render(request, 'news/index.html', context)
+
+def get_category(request, category_id):
+    post = Post.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {'news': post, 
+                'categories': categories, 
+                'category': category, }
+    return render(request, 'news/category.html', context)
