b3a811b1c3e31d6caca0e29bfb93825c5064e301
Denys Morozov <ajax3101@gmail.com>
2022-05-29 13:44:27 +0300

    custom template tags

---
+++ dualis/news/templatetags/news_tag.py
@@ -0,0 +1,15 @@
+from atexit import register
+from django import template
+from news.models import Category
+
+
+register = template.Library()
+
+@register.simple_tag(name='get_list_categories')
+def get_categories():
+    return Category.objects.all()
+
+@register.inclusion_tag('news/list_categories.html')
+def show_categories(arg1='World', arg2='Hello'):
+    categories = Category.objects.all()
+    return {"categories": categories, "arg1":arg1, "arg2":arg2}
--- dualis/news/views.py
+++ dualis/news/views.py
@@ -8,18 +8,13 @@ from .models import Post, Category
 
 def index(request):
     post = Post.objects.all()
-    categories = Category.objects.all()
-
-    context = {'news': post, 
-                'title': 'Список новин', 
-                'categories': categories, }
+    
+    context = {'news': post, 'title': 'Список новин', }
     return render(request, 'news/index.html', context)
 
 def get_category(request, category_id):
     post = Post.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
+    
     category = Category.objects.get(pk=category_id)
-    context = {'news': post, 
-                'categories': categories, 
-                'category': category, }
+    context = {'news': post, 'category': category, }
     return render(request, 'news/category.html', context)
