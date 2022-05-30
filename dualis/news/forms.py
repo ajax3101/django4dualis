9e049bad85d89440ca7463db836d404ef57943cd
Denys Morozov <ajax3101@gmail.com>
2022-05-30 22:47:29 +0300

    working with forms. part 2

--- dualis/news/forms.py
+++ dualis/news/forms.py
@@ -1,24 +1,14 @@
 from turtle import title
 from django import forms
-from .models import Category
+from .models import Category, Profile
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    subtitle = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
+    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={"class":"form-control"}))
+    subtitle = forms.CharField(max_length=255, label='Підзаголовок', required=False, widget=forms.TextInput(attrs={"class":"form-control"}))
+    #slug = forms.SlugField(max_length=255, label='Слаг', required=False, widget=forms.TextInput(attrs={"class":"form-control"}))
     #photo = forms.ImageField()
-    body = forms.CharField()
-    published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
-    
-
-    '''subtitle = models.CharField(max_length=255, blank=True, verbose_name = 'Підзаголовок')
-    slug = models.SlugField(max_length=255, unique=True, verbose_name = 'Слаг')
-    body = models.TextField(blank=True, verbose_name = 'Контент')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name = 'Фото')
-    meta_description = models.CharField(max_length=150, blank=True, verbose_name = 'Мета опис')
-    date_created = models.DateTimeField(auto_now_add=True, verbose_name = 'Дата створення')
-    date_modified = models.DateTimeField(auto_now=True, verbose_name = 'Оновлено')
-    publish_date = models.DateTimeField(blank=True, null=True, verbose_name = 'Дата публікування')
-    published = models.BooleanField(default=False, verbose_name = 'Опубліковано')
-    category''' 
+    body = forms.CharField(label='Контент', required=False, widget=forms.Textarea(attrs={"class":"form-control", "rows":"5"}))
+    published = forms.BooleanField(label='Опубліковано', initial=False)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категорія', empty_label='Оберіть категорію', widget=forms.Select(attrs={"class":"form-control"}))
+    author = forms.ModelChoiceField(queryset=Profile.objects.all(), label='Автор', empty_label='Оберіть Автора', widget=forms.Select(attrs={"class":"form-control"}))
+ 
--- dualis/news/views.py
+++ dualis/news/views.py
@@ -1,6 +1,6 @@
 from turtle import title
 from unicodedata import category
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Category
 from .forms import NewsForm
 
@@ -26,7 +26,11 @@ def view_news(request, news_id):
 
 def add_news(request):
     if request.method == 'POST':
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+ #           print(form.cleaned_data)
+            news = Post.objects.create(**form.cleaned_data)
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
