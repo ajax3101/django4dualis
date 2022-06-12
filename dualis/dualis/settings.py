0cc58bcd6b00e028b17273932918b0e4265f60d2
Denys Morozov <ajax3101@gmail.com>
2022-06-12 23:03:57 +0300

    добавил все формы

--- dualis/dualis/settings.py
+++ dualis/dualis/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'ckeditor',
     
     'news.apps.NewsConfig',
     'contact.apps.ContactConfig',
@@ -127,7 +129,9 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+CKEDITOR_UPLOAD_PATH = "uploads/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
