bc0fa3220b3b6bf9fe03e6465b5d60389fb04bdb
Denys Morozov <ajax3101@gmail.com>
2022-06-12 17:50:49 +0300

    startapp contact

--- dualis/dualis/settings.py
+++ dualis/dualis/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'news.apps.NewsConfig',
+    'contact.apps.ContactConfig',
 ]
 
 MIDDLEWARE = [
--- dualis/dualis/urls.py
+++ dualis/dualis/urls.py
@@ -23,6 +23,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
+    path('', include('contact.urls')),
    
 ]
 
